5bba243199507367a11b1ce45cbcf36031aadb24
James Moore <james@panic.com>
2017-11-30 12:21:47 -0800

    beginning pagination

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
+
 from flask import Flask
-from flask import g, session, request, url_for, flash
-from flask import redirect, render_template
+from flask import render_template
+from flask import session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
-import os
-from wb.models import metadata
+
+from wb.models import metadata, Rims
 
 app = Flask(__name__)
 config = os.environ.get('WB_CONFIG', 'config.DevelopmentConfig')
@@ -12,33 +14,44 @@ app.config.from_object(config)
 
 if app.config['SENTRY_DSN']:
     from raven.contrib.flask import Sentry
+
     sentry = Sentry(app, dsn=app.config['SENTRY_DSN'])
 
 db = SQLAlchemy(metadata=metadata)
 db.init_app(app)
 
+ITEMS_PER_PAGE = 20
 
 @app.before_request
 def before_request():
     try:
         db.engine.execute('SELECT 1 from rims')
     except exc.SQLAlchemyError as e:
-        return f"{e}    Wheelbuilder is unavailable at the moment", 503
+        return f"{e}<br>Wheelbuilder is unavailable at the moment", 503
 
     app.logger.info(session)
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html.j2')
+
+
+@app.route('/rims/list')
+@app.route('/rims/list/<int:page>')
+def rims_list(page=1):
+    rims = db.session.query(Rims).order_by(Rims.description.asc()).paginate(page=page,
+                                                                            per_page=ITEMS_PER_PAGE,
+                                                                            )
+
+    return render_template('_rims_table.html.j2',
+                           rims_paginated=rims)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-
     return render_template('404.html.j2'), 404
 
 
 if __name__ == '__main__':
-
     app.run()
