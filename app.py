0a43fe159a239be5d296aec5981dec773f2579e1
James Moore <james@panic.com>
2017-11-30 13:16:55 -0800

    rim filtering

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 
 from flask import Flask
-from flask import render_template
-from flask import session
+from flask import render_template, session, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
 
+from wb.forms import SearchForm
 from wb.models import metadata, Rims
 
 app = Flask(__name__)
@@ -21,6 +21,7 @@ db = SQLAlchemy(metadata=metadata)
 db.init_app(app)
 
 ITEMS_PER_PAGE = 20
+
 
 @app.before_request
 def before_request():
@@ -40,12 +41,29 @@ def index():
 @app.route('/rims/list')
 @app.route('/rims/list/<int:page>')
 def rims_list(page=1):
-    rims = db.session.query(Rims).order_by(Rims.description.asc()).paginate(page=page,
-                                                                            per_page=ITEMS_PER_PAGE,
-                                                                            )
+    rims = db.session.query(Rims)
+
+    form = SearchForm(request.args)
+    if form.validate():
+
+        size = form.size.data
+        search = form.search.data
+
+        if size:
+            rims = rims.filter(Rims.size == size)
+
+        if search:
+            rims = rims.filter(Rims.description.like(f"%{search}%"))
+
+    else:
+        for e in form.errors.items():
+            app.logger.error(e)
+
+    rims = rims.order_by(Rims.description.asc()).paginate(page=page, per_page=ITEMS_PER_PAGE)
 
     return render_template('_rims_table.html.j2',
-                           rims_paginated=rims)
+                           rims_paginated=rims,
+                           form=form)
 
 
 @app.errorhandler(404)
---
+++ wb/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField
+from wtforms.validators import Length, Regexp
+
+
+class SearchForm(FlaskForm):
+    class Meta:
+        csrf = False
+
+    forr = StringField(validators=[Length(max=1), Regexp(r'[fr]?')], default=None)
+    size = IntegerField(default=None)
+    search = StringField(default=None)
