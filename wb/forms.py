1c3db0674dcbefee170b0e2d59b85aabad8ea93b
james <james@panic.com>
2023-11-21 16:48:18 -0800

    update requirements

--- wb/forms.py
+++ wb/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import FloatField, SelectField, StringField
-from wtforms.fields.html5 import EmailField
+from wtforms.fields import EmailField
 from wtforms.validators import InputRequired, Optional, Email
 
 from wb.models import nipple_corrections, patterns, rim_sizes, spoke_counts
